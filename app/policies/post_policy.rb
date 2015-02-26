a2374bf1188cfa9f32bcaf43d0612b469a2937e1
Philippe Aka <philippeaka@outlook.com>
2015-02-25 22:49:00 -0500

    destroy, shallow nesting and more dry

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -14,7 +14,5 @@ class PostPolicy < ApplicationPolicy
   end
 
   def destroy?
-    user.present? && (record.user == user || user.admin? || user.moderator?)
+    user.present? && can_moderate?
   end
-  
-end
