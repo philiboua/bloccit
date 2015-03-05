f53da1cfb5357787338f1bec3acb5a18dbf8fa0e
Philippe Aka <philippeaka@outlook.com>
2015-03-04 23:06:41 -0500

    another interlude assignement helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,24 @@
 module ApplicationHelper
-   def form_group_tag(errors, &block)
-     if errors.any?
-       content_tag :div, capture(&block), class: 'form-group has-error'
-     else
-       content_tag :div, capture(&block), class: 'form-group'
-     end
-   end
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else
+      content_tag :div, capture(&block), class: 'form-group'
+    end
+  end
 
-   def markdown_to_html(markdown)
+  def markdown_to_html(markdown)
     renderer = Redcarpet::Render::HTML.new
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
- end
+  end
+
+  def up_vote_link(post)
+    "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current.user.voted(post).up_vote?) ? 'voted' : ""}"
+  end
+
+  def up_vote_link(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted':  ""}"
+  end
 end
