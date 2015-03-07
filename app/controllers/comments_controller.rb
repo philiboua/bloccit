f852c35da1f8e0bd38a2d35242b1ddb5d4fd332e
Philippe Aka <philippeaka@outlook.com>
2015-03-07 09:42:40 -0500

    destroying checkpoint

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,33 +5,38 @@ class CommentsController < ApplicationController
    #@comment = @post.comments.build(comment_params)
    #@comment.user_id = current_user.id
 
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
     @comments = @post.comments
    if @comment.save
       flash[:notice] = "Your comment was saved"
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     else
       flash[:error] = "There was an error saving your comment. Please try again."
-      redirect_to [@topic, @post]
+      redirect_to [@post.topic, @post]
     end
   end
 
   def destroy
-     @topic = Topic.find(params[:topic_id])
-     @post = @topic.posts.find(params[:post_id])
+   
+     @post = Post.find(params[:post_id])
      @comment = @post.comments.find(params[:id])
      authorize @comment
 
      if @comment.destroy
        flash[:notice] = "Comment was removed."
-       redirect_to [@topic, @post]
+   
      else
        flash[:error] = "Comment couldn't be deleted. Try again."
-       redirect_to [@topic, @post]
+     
      end
+
+     respond_to do |format|
+       format.html
+       format.js
+     end
+
    end
 
   private
