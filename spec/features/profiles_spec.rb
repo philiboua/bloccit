5356a2ad19c11df005b5b41bfc033a47a0643ba6
Philippe Aka <philippeaka@outlook.com>
2015-07-22 22:38:50 -0400

    changed profiles spec

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe "visiting profiles" do
 
   include TestFactories
+  include Warden::Test::Helpers
+  Warden.test_mode!
 
   before do 
     @user = authenticated_user
@@ -24,4 +26,24 @@ describe "visiting profiles" do
     end
 
   end
+  describe "user visit his own profile" do
+
+    before do 
+      @user = authenticated_user
+      @post = associated_post(user: @user)
+      @comment = @user.comments.build(post: @post, body: "A Comment")
+      allow(@comment).to receive(:send_favorite_emails)
+      login_as(@user, :scope => :user)
+    end
+
+    it "shows profile" do
+      visit user_path(@user)
+      expect(current_path).to eq (user_path(@user))
+ 
+      expect( page ).to have_content(@user.name)
+      expect( page ).to have_content(@post.title)
+      expect( page ).to have_content(@comment.body)
+    end
+  end
 end
+
